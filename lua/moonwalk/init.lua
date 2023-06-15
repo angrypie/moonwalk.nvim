64b96608082cdfac4f9ad32accefe3f7ea3f3eb4
Nikita Hrytsai <cookies.in.chocolate@gmail.com>
2023-06-15 20:58:25 +0300

    set extmarks on the begining of the treesitter node (scope where user is working now)

--- lua/moonwalk/init.lua
+++ lua/moonwalk/init.lua
@@ -1,5 +1,5 @@
+-- Use extmarks to mark scope
 -- Figure out scope-scoring algorithm (currently just score / i)
--- -
 -- While editing score should be higher
 -- Figure out frecency algorithm
 -- What is best max_scope_depth for scoring scope?
@@ -13,8 +13,8 @@ local M = {
 	scores          = {},
 	---@type table<string, integer>
 	marks           = {},
-	max_scope_depth = 20,
-	namespace       = vim.api.nvim_create_namespace('my-plugin')
+	max_scope_depth = 1,
+	ns              = vim.api.nvim_create_namespace('my-plugin')
 }
 
 
@@ -23,18 +23,21 @@ local timer = uv.new_timer()
 local debug_max_timer = 0
 
 -- This will wait 1000ms and then continue inside the callback
-timer:start(0, 1000, vim.schedule_wrap(function()
-	-- timer here is the value we passed in before from new_timer.
-
-	M.score_current_scope()
-
-	-- You must always close your uv handles or you'll leak memory
-	-- We can't depend on the GC since it doesn't know enough about libuv.
-	debug_max_timer = debug_max_timer + 1
-	if debug_max_timer == 1000 then
-		timer:close()
-	end
-end))
+if timer == nil then
+	-- throw error
+	vim.notify("moonwalk: luv timer is nil", vim.log.levels.ERROR)
+else
+	timer:start(0, 1000, vim.schedule_wrap(function()
+		-- timer here is the value we passed in before from new_timer.
+		M.score_current_scope()
+		-- You must always close your uv handles or you'll leak memory
+		-- We can't depend on the GC since it doesn't know enough about libuv.
+		debug_max_timer = debug_max_timer + 1
+		if debug_max_timer == 1000 then
+			timer:close()
+		end
+	end))
+end
 
 function M.score_current_scope()
 	local current = M.get_current_scope()
@@ -46,6 +49,34 @@ function M.score_current_scope()
 	M.score_nodes(current, M.max_scope_depth)
 end
 
+---TODO score_node is the most important part now, we have to figure out how to properly score each node.
+---Time of the visits should be taken into account. Possible look into exponential decay.
+---Also score_node sets extmarks and updatets them, sohuld be a lot of bugs while editing text.
+---=====
+---Increase score of the node, create extmark on the start of the node and returns new score.
+---If mark already exists on same line, it will be updated.
+---@param node TSNode
+---@param score integer
+---@return integer
+function M.score_node(node, score)
+	local line = node:range() -- node start position
+
+	-- get mark for line where ts node starts, or create new one
+	local mark = vim.api.nvim_buf_get_extmarks(0, M.ns, { line, 0 }, { line, -1 }, {})[1]
+	local id = -1
+	if mark == nil then
+		id = vim.api.nvim_buf_set_extmark(0, M.ns, line, -1, {})
+	else -- if mark exists, get id
+		id = mark[1]
+	end
+
+	-- update score of the mark
+	local old_score = M.scores[id] or 0
+	M.scores[id] = old_score + score
+
+	return M.scores[id]
+end
+
 ---Score all nodes towards root by specified depth. Chunk node is ignored.
 ---@param node TSNode
 ---@param depth integer
@@ -55,7 +86,7 @@ function M.score_nodes(node, depth)
 	local parent = current:parent()
 
 	-- traverse towards root but ignore "chunk" node
-	while parent ~= nil do
+	while parent ~= nil and depth ~= 0 do
 		table.insert(nodes, current)
 		current = parent
 		parent = current:parent()
@@ -69,26 +100,6 @@ function M.score_nodes(node, depth)
 		debug_str = debug_str .. n:type() .. string.format(" %.2f", new_score) .. " -> "
 	end
 	-- print(debug_str)
-end
-
----Increase score of the node and returns new score
----@param node TSNode
----@param score integer
----@return integer
-function M.score_node(node, score)
-	local id = node:id()
-
-	local old_score = M.scores[id] or 0
-	if old_score == nil then
-		M.scores[id] = 0
-	else
-		M.scores[id] = old_score + score
-	end
-
-	local node_start = node:range()[1]
-	local mark = vim.api.nivm_buf_set_extmark(0, M.namespace, nade_start[1], nade_start[2], {})
-
-	return old_score + score
 end
 
 ---Get node where user is likely working. Return nil if cursor on "cuhnk" node
@@ -107,22 +118,27 @@ end
 
 --sorts scores and moves cursor to the first node
 function M.walk_to_best_mark()
-	local top = nil
+	local top = 0
 	local value = 0
-	for k, v in pairs(M.scores) do
+	for k, v in ipairs(M.scores) do
 		if v > value then
 			top = k
 			value = v
 		end
 	end
-	local mark = vim.api.nvim_buf_get_extmark_by_id(0, M.namespace, M.marks[top], {})[1]
-	--check if mark empty list
+	if top == 0 then
+		vim.notify("moonwalk: no history available", vim.log.levels.ERROR)
+		return
+	end
+	-- convert top to integer id
+	local mark = vim.api.nvim_buf_get_extmark_by_id(0, M.ns, top, {})
 
 	if mark == nil then
 		print("No marks found")
 		return
 	end
-	vim.api.nvim_win_set_cursor(0, { mark[1], mark[2] + 1 })
+	print("mark", mark[1], mark[2], value, top)
+	vim.api.nvim_win_set_cursor(0, { mark[1] + 1, mark[2] })
 end
 
 return M
--- plugin/plugin.lua
+++ plugin/plugin.lua
@@ -1,20 +1,19 @@
--- local m = require('moonwalk')
+local m = require('moonwalk')
 
--- vim.keymap.set('n', '\\', m.walk_to_top, { noremap = true, silent = true })
--- vim.keymap.set('n', '<C-s>', m.walk_to_top, { noremap = true, silent = true })
+vim.keymap.set('n', '<C-s>', m.walk_to_best_mark, { noremap = true, silent = true })
 
 
-local ffi = require 'ffi'
+-- local ffi = require 'ffi'
 
-ffi.cdef [[
-	int get_magenta()
-]]
+-- ffi.cdef [[
+-- 	int get_magenta()
+-- ]]
 
-local lib = ffi.load('./libmain.dylib')
+-- local lib = ffi.load('./libmain.dylib')
 
-vim.keymap.set('n', '<C-s>', function()
-	local result = lib.get_magenta()
-	print(result)
-end, {})
+-- vim.keymap.set('n', '<C-s>', function()
+-- 	local result = lib.get_magenta()
+-- 	print(result)
+-- end, {})
 
 -- print(lib.get_magenta())
